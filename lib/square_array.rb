df4ee94ac4a05ac66cb30631332d4c6be656503a
Gerald Tee <93geraldtee@gmail.com>
2019-09-17 21:01:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  array = [1,2,3]
+  array1 = [1,2,3]
   
-  while array[array] do
-   puts array[array]**2
-   array += 1
+  while array[array1] do
+   puts array[array1]**2
+   array1 += 1
  end
 end
